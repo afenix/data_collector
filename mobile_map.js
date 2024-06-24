aa14ab11ab9faac635192431f19bb45dd1a2d57a
afenix <alister.fenix@gmail.com>
2024-06-24 15:02:05 -0700

    Add Locate widget to map

--- mobile_map.js
+++ mobile_map.js
@@ -1,5 +1,5 @@
 // Load the AMD modules from the ESRI API
-require(['esri/Map', 'esri/views/MapView', 'esri/config'], (Map, MapView, esriConfig) => {
+require(['esri/Map', 'esri/views/MapView', 'esri/config', 'esri/widgets/Locate'], (Map, MapView, esriConfig, Locate) => {
 
     // Set the API key (make sure it's restricted to the domain of the application)
     esriConfig.apiKey = "AAPK83337061f79941cdbcba8ea16add7f1csWFIvmrzXU7TvesGSEbfGqhfxRivSP37KmfuCDfiec8kVrxhDCre40EzzsvFCLSB";
@@ -16,5 +16,15 @@ require(['esri/Map', 'esri/views/MapView', 'esri/config'], (Map, MapView, esriCo
       center: [-105.0166, 39.6133], // The center of the map as lon/lat for Littleton, Colorado
       zoom: 13                      // Sets the zoom level of detail (LOD) to 13
     })
+
+    const locateBtn = new Locate({
+      view: view
+    });
+
+    // Add the locate widget to the top left corner of the view
+    view.ui.add(locateBtn, {
+      position: "top-left"
+    });
+
 
 })
